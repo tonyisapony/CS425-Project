4a1ce40e580b0950448fce7127a8b348812f9eb8
tonyisapony <patlantony@hotmail.com>
2015-05-06 20:14:06 -0500

    Tony added new methods.
    Will continue to work on this late if you still haven't gotten to it.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.sql.*;
 import java.text.DecimalFormat;
 import oracle.jdbc.pool.OracleDataSource;
@@ -35,16 +36,29 @@ public class Main {
 		    	String usrname=scan.next();
 		    	System.out.println("Enter new password:");
 		    	String passwrd=scan.next();
-		    	sql="INSERT INTO Membership (id,name,password) VALUES ("+currid+",'"+usrname+"','"+passwrd+"')";
+		    	System.out.println("Enter email address");
+		    	String email= scan.next();
+		    	sql="INSERT INTO Membership (id,name,password,email) VALUES ("+currid+",'"+usrname+"','"+passwrd+"','"+email+"')";
 		    	stmt.executeUpdate(sql);
 		    	System.out.println("Successfully registered");
+		    	System.out.println("You are now logged in please choose what interest group you would like to access. \n 1:Restaurants 2:Laptops");
+		    	int in1=scan.nextInt();
+		    	if (in1==1)
+		    	{
+		    		Restaurants();
+		    	}
+		    	else if(in1==2)
+		    	{
+		    		Laptops();
+		    	}
+		    
 		    	
 		    }
 		    else if(in==2){
 		    	boolean logged=false;
-		    	System.out.println("Enter new user name:");
+		    	System.out.println("Enter user name:");
 		    	String usrname=scan.next();
-		    	System.out.println("Enter new password:");
+		    	System.out.println("Enter password:");
 		    	String passwrd=scan.next();
 		    	sql="SELECT name,password FROM Membership";
 		    	rs=stmt.executeQuery(sql);
@@ -55,6 +69,16 @@ public class Main {
 		    		String p[]=ps.split(" ",2);
 		    		if(n[0].equals(usrname)&&p[0].equals(passwrd)){
 		    			System.out.println("You have successfully logged in");
+		    			System.out.println("You are now logged in please choose what interest group you would like to access. \n 1:Restaurants 2:Laptops");
+				    	int in1=scan.nextInt();
+				    	if (in1==1)
+				    	{
+				    		Restaurants();
+				    	}
+				    	else if(in1==2)
+				    	{
+				    		Laptops();
+				    	}
 		    			logged=true;
 		    			break;
 		    		}
@@ -88,6 +112,50 @@ public class Main {
 		      }catch(SQLException se){
 		         se.printStackTrace();
 		      }//end finally try
+	}
+	
+	
+	//TONY WORKING ON THESE METHODS... RESTAURANT AND LAPTOP COMMENTS
+	public static void Restaurants()
+	{
+		System.out.println("You have now been signed into the Restaurant Interest Group.");
+		Connection con=null;
+		String sql=null;
+		Scanner scan=new Scanner(System.in);			
+		ResultSet rs=null;
+		Statement stmt=null;
+			try{	
+			OracleDataSource ds=new OracleDataSource();
+			ds.setURL(host);
+			con=ds.getConnection(uName,uPass);
+			stmt=con.createStatement();
+			sql = "SELECT * FROM REVIEW";
+			System.out.println("Now listing all restaurants: \n"+ sql);
+			}
+			catch(SQLException err){
+				System.out.println(err.getMessage());
+			}finally{
+			      //finally block used to close resources
+			      try{
+			            stmt.close();
+			      }catch(SQLException se){
+			      }// do nothing
+			      scan.close();
+			      try{
+			    	  rs.close();
+			 	  }catch(SQLException se){
+			 	  }// do nothing
+			      }
+			      try{
+			          con.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }//end finally try
+	}
+	
+	public static void Laptops()
+	{
+		
 	}
 }
 
